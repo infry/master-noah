398ceda03de5a27eaa56d6da6601bb1a8ae8b946
noxhy <58011428+noxhy@users.noreply.github.com>
2026-05-08 22:19:07 -0400

    helper function setup

--- scenes/alphabet.gd
+++ scenes/alphabet.gd
@@ -1,11 +1,18 @@
 @tool
 extends Node2D
 
+@export_group("Text Settings")
 @export_multiline() var text:String = "":
 	set(value):
 		text = value
 		update_text(value)
 
+@export var horizontal_alignment: HorizontalAlignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_LEFT:
+	set(v):
+		horizontal_alignment = v
+		update_text(text)
+
+@export_group("Glyph Settings")
 @export var sprite_frames:SpriteFrames = preload("res://assets/fonts/alphabet.res"):
 	set(value):
 		sprite_frames = value
@@ -17,33 +24,23 @@ extends Node2D
 		forced_anim_suffix = value
 		update_text(text)
 
-func get_suffix(symbol:String):
-	if forced_anim_suffix != "" and sprite_frames.has_animation(symbol + forced_anim_suffix):
-		return forced_anim_suffix
-	if sprite_frames.has_animation(symbol):
-		return ""
-	if symbol == symbol.to_upper() and sprite_frames.has_animation(symbol + " capital"):
-		return " capital"
-	if symbol == symbol.to_lower() and sprite_frames.has_animation(symbol + " lowercase"):
-		return " lowercase"
-	return ""
-
 @export var line_gap:float = 70.0:
 	set(value):
 		line_gap = value
 		update_text(value)
-		
-@export var default_symbol_width:float = 0.0:
+
+@export var default_character_gap:float = 0.0:
 	set(value):
-		default_symbol_width = value
+		default_character_gap = value
 		update_text(text)
 
-@export var symbol_widths:Dictionary[String, float] = { " ": 40.0 }:
+@export_subgroup("Overrides")
+@export var character_gap:Dictionary[String, float] = { " ": 40.0 }:
 	set(value):
-		symbol_widths = value
+		character_gap = value
 		update_text(text)
-		
-@export var symbol_offsets:Dictionary[String, Vector2] = {
+
+@export var character_offsets:Dictionary[String, Vector2] = {
 	"g": Vector2(0, 15),
 	"j": Vector2(0, 15),
 	"p": Vector2(0, 15),
@@ -51,83 +48,114 @@ func get_suffix(symbol:String):
 	"y": Vector2(0, 15)
 }:
 	set(value):
-		symbol_offsets = value
+		character_offsets = value
 		update_text(text)
 
-@export var symbol_name_overrides:Dictionary[String, String] = {
-	"&": "amp",
-	"😠": "angry faic",
-	"'": "apostraphie",
-	",": "comma",
-	"$": "dollarsign",
-	"↓": "down arrow",
-	"”": "end parentheses",
-	"!": "exclamation point",
-	"/": "forward slash",
-	"#": "hashtag ",
-	"♥": "heart",
-	"♡": "heart",
-	"←": "left arrow",
-	"*": "multiply x",
-	".": "period",
-	"?": "question mark",
-	"→": "right arrow",
-	"“": "start parentheses",
-	"↑": "up arrow",
+@export var glyph_name_overrides:Dictionary[String, StringName] = {
+	"&": &"amp",
+	"😠": &"angry faic",
+	"'": &"apostraphie",
+	",": &"comma",
+	"$": &"dollarsign",
+	"↓": &"down arrow",
+	"”": &"end parentheses",
+	"!": &"exclamation point",
+	"/": &"forward slash",
+	"#": &"hashtag ",
+	"♥": &"heart",
+	"♡": &"heart",
+	"←": &"left arrow",
+	"*": &"multiply x",
+	".": &"period",
+	"?": &"question mark",
+	"→": &"right arrow",
+	"“": &"start parentheses",
+	"↑": &"up arrow",
 }:
 	set(value):
-		symbol_name_overrides = value
+		glyph_name_overrides = value
 		update_text(text)
 
-func get_symbol_sprite_frames_name(symbol:String):
-	if symbol in symbol_name_overrides:
-		symbol = symbol_name_overrides[symbol]
-	return symbol + get_suffix(symbol)
+
+func get_suffix(character:String) -> StringName:
+	if forced_anim_suffix != &"" and sprite_frames.has_animation(character + forced_anim_suffix):
+		return forced_anim_suffix
+	if sprite_frames.has_animation(character):
+		return &""
+	if character == character.to_upper() and sprite_frames.has_animation(character + &" capital"):
+		return &" capital"
+	if character == character.to_lower() and sprite_frames.has_animation(character + &" lowercase"):
+		return &" lowercase"
 	
+	return &""
+
+
+func get_glyph_name(character: String) -> StringName:
+	return glyph_name_overrides.get(character, character) + get_suffix(character)
+
+func get_glyph_texture(glyph: String) -> Texture2D:
+	if sprite_frames and sprite_frames.has_animation(glyph):
+		return sprite_frames.get_frame_texture(glyph, 0)
+	
+	return null
+
+## Returns the width of the string's characters
+func get_string_width(line: String) -> float:
+	var sum: float = 0
+	
+	for c in line:
+		var glyph_name: StringName = get_glyph_name(c)
+		var glyph_texture: Texture2D = get_glyph_texture(glyph_name)
+		if glyph_texture:
+			sum += glyph_texture.get_width()
+		
+		sum += character_gap.get(c, default_character_gap)
+	
+	return sum
+
+
 func update_text(new_text):
-	for letter in get_children():
-		letter.queue_free()
+	for glyph in get_children():
+		glyph.queue_free()
 	
-	var letters = new_text.split()
+	if new_text.is_empty():
+		return
+	
+	var characters = new_text.split()
 	
 	var next_x:float
-	for i in letters.size():
-		var symbol = letters[i]
-		var letter:AnimatedSprite2D = AnimatedSprite2D.new()
-		letter.sprite_frames = sprite_frames
-		letter.centered = false
+	
+	match horizontal_alignment:
+		HorizontalAlignment.HORIZONTAL_ALIGNMENT_CENTER:
+			next_x = -get_string_width(new_text) / 2
 		
-		var anim_name = get_symbol_sprite_frames_name(symbol)
-		if letter.sprite_frames.has_animation(anim_name):
-			letter.play(anim_name)
-			letter.sprite_frames.set_animation_loop(anim_name, true)
-
-		letter.set_meta("symbol", symbol)
-		letter.set_meta("sprite_frames_anim", anim_name)
-		letter.offset = symbol_offsets.get(symbol) if symbol in symbol_offsets.keys() else Vector2(0.0, 0.0)
-		self.add_child(letter)
+		HorizontalAlignment.HORIZONTAL_ALIGNMENT_RIGHT:
+			next_x = -get_string_width(new_text)
 		
-		if i > 0: # TODO: xPos and yPos this bitch bro we need multi line support.
-			letter.position.x = next_x
-		else:
-			letter.position.x = i * 40
+		_:
+			next_x = 0
+	
+	for i in characters.size():
+		var character: String = characters[i]
+		var glyph: AnimatedSprite2D = AnimatedSprite2D.new()
+		glyph.sprite_frames = sprite_frames
+		glyph.centered = false
 		
-		next_x = letter.position.x
-		if symbol in symbol_widths.keys():
-			next_x += symbol_widths.get(symbol)
-		else:
-			next_x += default_symbol_width
+		var glyph_name: StringName = get_glyph_name(character)
+		if sprite_frames.has_animation(glyph_name):
+			glyph.play(glyph_name)
+			sprite_frames.set_animation_loop(glyph_name, true)
 		
-		var frame_tex
-		if letter.sprite_frames:
-			frame_tex = letter.sprite_frames.get_frame_texture(letter.animation, 0)
+		glyph.offset = character_offsets.get(character, Vector2(0.0, 0.0))
+		self.add_child(glyph)
 		
-		if frame_tex:
-			var real_width = frame_tex.get_width()
-			if real_width:
-				next_x += real_width
-			
-			var real_height = frame_tex.get_height()
-			if real_height:
-				letter.position.y -= real_height
+		glyph.position.x = next_x
 		
+		next_x = glyph.position.x
+		next_x += character_gap.get(character, default_character_gap)
+		
+		var glyph_texture: Texture2D = get_glyph_texture(glyph_name)
+		
+		if glyph_texture:
+			next_x += glyph_texture.get_width()
+			glyph.position.y -= glyph_texture.get_height()
