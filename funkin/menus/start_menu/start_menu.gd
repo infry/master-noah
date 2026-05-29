5cedd5a5d3059599a429f3dbf022b88bea9a1887
noxhy <58011428+noxhy@users.noreply.github.com>
2026-05-28 20:34:24 -0400

    Update start_menu.gd

--- funkin/menus/start_menu/start_menu.gd
+++ funkin/menus/start_menu/start_menu.gd
@@ -48,18 +48,18 @@ func _on_conductor_new_beat(current_beat, measure_relative):
 		%Metronome.can_dance = true
 		%Metronome.play_animation("idle", $Conductor.seconds_per_beat * 2)
 	
-	var tween = create_tween()
-	tween.set_parallel(true)
 	if intro_done:
-			# Background color phase
-			$Background/ColorRect.color = flashing_colors[current_beat % flashing_colors.size()]
-			tween.tween_property($Background/ColorRect, "color", Color(0, 0, 0), 0.3)
-			
-			# "Press enter" text color phasing, i most definitely could've done a sine equation
-			if(current_beat % 2 == 0):
-				tween.tween_property($"UI/Play Label", "theme_override_colors/font_color", Color(0, 0.366667, 1, 0.8), $Conductor.seconds_per_beat * 2)
-			else:
-				tween.tween_property($"UI/Play Label", "theme_override_colors/font_color", Color(0.501961, 0.682353, 1, 0.8), $Conductor.seconds_per_beat * 2)
+		var tween = create_tween()
+		tween.set_parallel(true)
+		# Background color phase
+		$Background/ColorRect.color = flashing_colors[current_beat % flashing_colors.size()]
+		tween.tween_property($Background/ColorRect, "color", Color(0, 0, 0), 0.3)
+		
+		# "Press enter" text color phasing, i most definitely could've done a sine equation
+		if(current_beat % 2 == 0):
+			tween.tween_property($"UI/Play Label", "theme_override_colors/font_color", Color(0, 0.366667, 1, 0.8), $Conductor.seconds_per_beat * 2)
+		else:
+			tween.tween_property($"UI/Play Label", "theme_override_colors/font_color", Color(0.501961, 0.682353, 1, 0.8), $Conductor.seconds_per_beat * 2)
 	Global.bop_tween($UI/Logo, "scale", Vector2(0.95, 0.95), Vector2(0.9, 0.9), 0.2, Tween.TRANS_QUAD)
 
 
